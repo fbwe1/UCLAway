6af1e64c59b7f33d7a9cd17e5ad1cd2155cea633
thomas lien <thomaschilien@gmail.com>
2026-05-31 23:39:07 -0700

    Restore messages route import to be consistent with main

--- backend/routes/messages.js
+++ backend/routes/messages.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const supabase = require('../supabaseclient');
+const supabase = require('../supabaseClient');
 
 /*
   DIRECT MESSAGES ARCHITECTURE
