49639d71bfcdbcd0633bf68e59b4a2c258fddaaf
thomas lien <thomaschilien@gmail.com>
2026-06-02 15:10:23 -0700

    Create verification workflow feature for JWT middleware

--- backend/middlewares/jwtMiddlewares.js
+++ backend/middlewares/jwtMiddlewares.js
@@ -8,4 +8,13 @@ module.exports = (req, res, next) => {
     const token = authHeader.split(" ")[1];
     if (!token) 
         return res.status(401).json({message: "Malformed/invalid token"})
+    // verify -> attach decoded user data to req. obj. -> move on
+    try {
+        const decoded = verifyToken(token);
+        req.user = decoded;
+        next();
+    } catch{ 
+        // if invalid or expired token
+        res.status(401).json({ message: "Invalid/expired token"})
+    }
 }
