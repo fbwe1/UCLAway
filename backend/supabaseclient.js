5debe9ebaea38dd383fd4654915aa2fb08181dec
thomas lien <thomaschilien@gmail.com>
2026-06-04 00:53:16 -0700

    Add JWT to RideFeed.jsx

--- backend/supabaseclient.js
+++ backend/supabaseclient.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 
 const supabase = createClient(
     process.env.SUPABASE_URL,
-    process.env.SUPABASE_KEY || process.env.SUPABASE_SERVICE_ROLE_KEY,
+    process.env.SUPABASE_SERVICE_ROLE_KEY || process.env.SUPABASE_KEY,
     {
         auth: {
             autoRefreshToken: false,
